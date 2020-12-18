56ad597e14988cf1e665bb30a0054b0ed677a227
Wael-Hx <wael.lhb@gmail.com>
2020-12-18 18:28:18 +0100

    navbar

---
+++ src/components/nav/Navbar.tsx
@@ -0,0 +1,11 @@
+import StyledButton from "../styled/StyledButton";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <StyledButton> Sign In </StyledButton>
+    </nav>
+  );
+};
+
+export default Navbar;
