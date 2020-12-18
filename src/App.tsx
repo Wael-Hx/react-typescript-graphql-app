b1fa1fc2cf6771fe7ed00b4aa14abe098a8d0fda
Wael-Hx <wael.lhb@gmail.com>
2020-12-18 22:38:58 +0100

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import Register from "./components/auth/Register";
+import Navbar from "./components/nav/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
-  return <div className="App">Hello</div>;
+  return (
+    <Router>
+      <Route path="/" component={Navbar} />
+      <Switch>
+        <Route path="/auth" component={Register} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
