e86dd8f61cc0622abd377c0d3305818bfe7bf519
Wael-Hx <wael.lhb@gmail.com>
2020-12-24 13:23:40 +0100

    navbar component

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -1,12 +1,64 @@
-import { Link } from "react-router-dom";
+import { Link, RouteComponentProps } from "react-router-dom";
 import StyledButton from "../styled/StyledButton";
+import { useMutation, useQuery } from "@apollo/client";
+import { ME, USER } from "../../gql/queries/users";
+import { LOGOUT } from "../../gql/mutations/users";
+import { loggedUserVar, UserData } from "../../cache";
+import { FC, useEffect } from "react";
 
-const Navbar = () => {
+const Navbar: FC<RouteComponentProps> = ({ history }) => {
+  const { loading, data, refetch } = useQuery(ME, {
+    fetchPolicy: "network-only",
+  });
+  const [logoutUser, { loading: logoutLoading, client }] = useMutation(LOGOUT);
+  const { data: userData } = useQuery<UserData>(USER);
+
+  useEffect(() => {
+    if (data?.me) {
+      loggedUserVar({
+        isAuthenticated: true,
+        user: data.me,
+      });
+    }
+  }, [data]);
+
+  const logout = async () => {
+    try {
+      await logoutUser();
+      refetch();
+      loggedUserVar({
+        isAuthenticated: false,
+        user: null,
+      });
+      client.cache.reset();
+      history.push("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <nav>
-      <Link className="link" to="/auth">
-        <StyledButton> Sign In </StyledButton>
-      </Link>
+      {userData?.loggedUser.isAuthenticated ? (
+        <StyledButton onClick={logout} disabled={logoutLoading}>
+          logout
+        </StyledButton>
+      ) : (
+        <Link className="link" to="/auth">
+          <StyledButton disabled={loading}> Sign In </StyledButton>
+        </Link>
+      )}
+
+      {userData?.loggedUser.user && (
+        <>
+          <Link className="link" to="/profile">
+            <StyledButton>Profile</StyledButton>
+          </Link>
+          <Link className="link" to="/">
+            <StyledButton>Home</StyledButton>
+          </Link>
+        </>
+      )}
     </nav>
   );
 };
