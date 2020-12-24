3e3a379ea821aab4df35f667de301cca5b63531c
Wael-Hx <wael.lhb@gmail.com>
2020-12-24 10:25:54 +0100

    users mutation profile

--- src/gql/mutations/users.ts
+++ src/gql/mutations/users.ts
@@ -7,6 +7,8 @@ export const REGISTER = gql`
     ) {
       id
       username
+      email
+      createdAt
     }
   }
 `;
@@ -16,6 +18,55 @@ export const LOGIN = gql`
     login(credentials: { email: $email, password: $password }) {
       id
       username
+      email
+      createdAt
+    }
+  }
+`;
+
+export const LOGOUT = gql`
+  mutation Logout {
+    logout
+  }
+`;
+export const CREATE_PROFILE = gql`
+  mutation CreateProfile($bio: String, $avatar: String) {
+    createProfile(userProfile: { bio: $bio, avatar: $avatar }) {
+      user {
+        username
+        createdAt
+        updatedAt
+      }
+      bio
+      avatar
+      saved {
+        id
+        title
+        images
+      }
+      posts {
+        id
+        title
+        images
+      }
+      likes {
+        id
+        title
+        images
+      }
+    }
+  }
+`;
+
+export const UPDATE_PROFILE = gql`
+  mutation CreateProfile($bio: String, $avatar: String) {
+    createProfile(userProfile: { bio: $bio, avatar: $avatar }) {
+      user {
+        username
+        updatedAt
+      }
+      bio
+      avatar
     }
   }
 `;
