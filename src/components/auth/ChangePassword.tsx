6d847506fff08a339dfed9f7b00b3d933fd1c3a3
Wael-Hx <wael.lhb@gmail.com>
2021-01-16 08:48:31 +0100

    redirect if user exists

--- src/components/auth/ChangePassword.tsx
+++ src/components/auth/ChangePassword.tsx
@@ -1,13 +1,16 @@
-import { TextField } from "@material-ui/core";
+import { CircularProgress, TextField } from "@material-ui/core";
 import { ChangeEvent, FormEvent, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Redirect, useParams } from "react-router-dom";
 import StyledButton from "../styled/StyledButton";
 import StyledForm from "../styled/StyledForm";
+import { loggedUserVar, UserVar } from "../../cache";
+import { useReactiveVar } from "@apollo/client";
 
 const ChangePassword = () => {
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const { token } = useParams<{ token: string }>();
+  const auth = useReactiveVar<UserVar>(loggedUserVar);
 
   const handlePassChange = (e: ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
@@ -18,31 +21,44 @@ const ChangePassword = () => {
   const changeYourPassword = (e: FormEvent) => {
     e.preventDefault();
   };
+
+  if (auth.loading) {
+    return (
+      <div className="center">
+        <CircularProgress />
+      </div>
+    );
+  }
+  if (auth.user) {
+    return <Redirect to="/" />;
+  }
   return (
-    <StyledForm onSubmit={changeYourPassword} width="20%" topMargin="10%">
-      {token ? (
-        <TextField
-          label="Reset Password"
-          name="password"
-          onChange={handlePassChange}
-          value={password}
-          type="password"
-          required
-          autoComplete="off"
-        />
-      ) : (
-        <TextField
-          label="Type your Email"
-          name="email"
-          value={email}
-          onChange={handmeEmailChange}
-          type="email"
-          required
-          autoComplete="on"
-        />
-      )}
-      <StyledButton type="submit">Confirm</StyledButton>
-    </StyledForm>
+    <section className="slide-overflow">
+      <StyledForm onSubmit={changeYourPassword} width="20%" topMargin="10%">
+        {token ? (
+          <TextField
+            label="Reset Password"
+            name="password"
+            onChange={handlePassChange}
+            value={password}
+            type="password"
+            required
+            autoComplete="off"
+          />
+        ) : (
+          <TextField
+            label="Type your Email"
+            name="email"
+            value={email}
+            onChange={handmeEmailChange}
+            type="email"
+            required
+            autoComplete="on"
+          />
+        )}
+        <StyledButton type="submit">Confirm</StyledButton>
+      </StyledForm>
+    </section>
   );
 };
 
